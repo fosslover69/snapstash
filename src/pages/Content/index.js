0b2df468488a131ee982b56ebcbf731c7e52f927
fosslover <no.1opensourcelover@protonmail.com>
2023-05-17 19:43:16 +0530

    snapstash: content: add notifications [2/3]

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -2,10 +2,24 @@ let active = false;
 document.addEventListener('keydown', function (event) {
   if (event.ctrlKey && event.key === ';') {
     active = !active;
+    title = active ? 'Extension Activated' : 'Extension Deactivated';
+    message = active
+      ? 'Select the text and press Ctrl + Y to save the text'
+      : 'Press Ctrl + ; to activate the extension again';
+    chrome.runtime.sendMessage({
+      type: 'notification',
+      title: title,
+      message: message,
+    });
     if (active) {
       document.addEventListener('keydown', function (event) {
         if (event.ctrlKey && event.key === 'y') {
           handleSelection();
+          chrome.runtime.sendMessage({
+            type: 'notification',
+            title: 'Content Saved',
+            message: 'Selected text saved successfully',
+          });
         }
       });
     }
