f3c52b76033bc3ed833d43af97f80fc531c121ed
fosslover <no.1opensourcelover@protonmail.com>
2023-05-17 20:49:59 +0530

    snapstash: popup: add delete functionality

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -17,6 +17,13 @@ const Popup = () => {
     });
   }
 
+  function removeStash(id) {
+    const newStashes = stashes.filter((stash) => stash.id !== id);
+    chrome.storage.local.set({ selectedArray: newStashes }, function () {
+      setStashes(newStashes);
+    });
+  }
+
   async function getCurrentTab() {
     let queryOptions = {
       active: true,
@@ -63,6 +70,23 @@ const Popup = () => {
                 <p>{stash.site}</p>
                 <p>{stash.date}</p>
               </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="close-button"
+                onClick={() => {
+                  removeStash(stash.id);
+                }}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
             </div>
           ))}
         </div>
