5f6e06a4e354fab3bc8aac8b1b465050b3a2a052
fosslover <no.1opensourcelover@protonmail.com>
2023-05-17 19:39:51 +0530

    snapstash: background: add save snaps to storage functionality [1/3]

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,24 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+function saveText(text, url, date) {
+  let urlName = url.match(
+    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/
+  )[1];
+  date = date.toLocaleDateString('in');
+  chrome.storage.local.get(['selectedArray'], function (result) {
+    let storedArray = result.selectedArray || [];
+    const selectObject = {
+      id: storedArray.length,
+      site: urlName,
+      date: date,
+      content: text,
+      url: url,
+    };
+    storedArray.push(selectObject);
+    chrome.storage.local.set({ selectedArray: storedArray });
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === 'saveText') {
+    saveText(request.text, request.url, request.date);
+  }
+});
