bda0bbd3bd9ff838acd1166edde64ce71ff221b4
fosslover <no.1opensourcelover@protonmail.com>
2023-05-19 12:53:45 +0530

    snapstash: popup: add icons

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Popup.css';
 import { useState, useEffect } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import icon from '../../assets/img/icon-128.png';
 
 const Popup = () => {
   // Stash States
@@ -158,6 +159,7 @@ const Popup = () => {
 
       {/* Extension Popup */}
       <header className="App-header">
+        <img src={icon} width={'40px'} alt="logo" />
         <h1>SnapStash</h1>
       </header>
       <div className="stash">
@@ -165,12 +167,31 @@ const Popup = () => {
           <h2>Your Stashes:</h2>
           <button
             id="idkbutton"
-            className={`stash-header-button ${
+            className={`stash-header-button stash-enhance-button ${
               filter !== '' ? 'stash-header-active-button' : ''
             }`}
             onClick={() => filterStash()}
           >
-            Current Site
+            <svg
+              width="20px"
+              height="20px"
+              viewBox="0 0 64 64"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke-width="2"
+              stroke="#ffffff"
+              fill="none"
+            >
+              <path d="M39.93,55.72A24.86,24.86,0,1,1,56.86,32.15a37.24,37.24,0,0,1-.73,6" />
+              <path d="M37.86,51.1A47,47,0,0,1,32,56.7" />
+              <path d="M32,7A34.14,34.14,0,0,1,43.57,30a34.07,34.07,0,0,1,.09,4.85" />
+              <path d="M32,7A34.09,34.09,0,0,0,20.31,32.46c0,16.2,7.28,21,11.66,24.24" />
+              <line x1="10.37" y1="19.9" x2="53.75" y2="19.9" />
+              <line x1="32" y1="6.99" x2="32" y2="56.7" />
+              <line x1="11.05" y1="45.48" x2="37.04" y2="45.48" />
+              <line x1="7.14" y1="32.46" x2="56.86" y2="31.85" />
+              <path d="M53.57,57,58,52.56l-8-8,4.55-2.91a.38.38,0,0,0-.12-.7L39.14,37.37a.39.39,0,0,0-.46.46L42,53.41a.39.39,0,0,0,.71.13L45.57,49Z" />
+            </svg>
+            <p>Current Site</p>
           </button>
         </div>
         {filter !== '' ? <p>Showing stashes from {filter}</p> : null}
@@ -191,11 +212,22 @@ const Popup = () => {
               <div className="stash-details">
                 <p>{stash.site}</p>
                 <button
-                  className="stash-header-button stash-header-active-button"
+                  className="stash-header-button stash-header-active-button stash-enhance-button"
                   style={{ fontSize: '0.7rem' }}
                   onClick={() => setGptText(stash.content)}
                 >
-                  Enhance
+                  <svg
+                    fill="#ffffff"
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 32 32"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>magic</title>
+                    <path d="M9.5 9.625l-0.906 2.906-0.875-2.906-2.906-0.906 2.906-0.875 0.875-2.938 0.906 2.938 2.906 0.875zM14.563 8.031l-0.438 1.469-0.5-1.469-1.438-0.469 1.438-0.438 0.5-1.438 0.438 1.438 1.438 0.438zM0.281 24l17.906-17.375c0.125-0.156 0.313-0.25 0.531-0.25 0.281-0.031 0.563 0.063 0.781 0.281 0.094 0.063 0.219 0.188 0.406 0.344 0.344 0.313 0.719 0.688 1 1.063 0.125 0.188 0.188 0.344 0.188 0.5 0.031 0.313-0.063 0.594-0.25 0.781l-17.906 17.438c-0.156 0.156-0.344 0.219-0.563 0.25-0.281 0.031-0.563-0.063-0.781-0.281-0.094-0.094-0.219-0.188-0.406-0.375-0.344-0.281-0.719-0.656-0.969-1.063-0.125-0.188-0.188-0.375-0.219-0.531-0.031-0.313 0.063-0.563 0.281-0.781zM14.656 11.375l1.313 1.344 4.156-4.031-1.313-1.375zM5.938 13.156l-0.406 1.438-0.438-1.438-1.438-0.469 1.438-0.438 0.438-1.469 0.406 1.469 1.5 0.438zM20.5 12.063l0.469 1.469 1.438 0.438-1.438 0.469-0.469 1.438-0.469-1.438-1.438-0.469 1.438-0.438z"></path>
+                  </svg>
+                  <p>Enhance</p>
                 </button>
                 <p>{stash.date}</p>
               </div>
